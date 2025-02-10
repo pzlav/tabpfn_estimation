351e50d6e106a05973cec8452f2ff20c52684a42
pzla <pzlacfa@gmail.com>
2025-02-10 18:22:18 +0300

    Initial commit

--- model.py
+++ model.py
@@ -1,10 +1,111 @@
+import pandas as pd
+from tabpfn import TabPFNRegressor
+from catboost import CatBoostRegressor, Pool
+import warnings
+warnings.filterwarnings('ignore')
+from tqdm import tqdm
+
 class Model:
-    def __init__(self):
-        self.mean_rate = None
 
-    def fit(self, x, y):
-        self.mean_rate = y.mean()
-        return self
+    def __init__(self, catboost_params=None, cat_cols=None):
+        if catboost_params is None:
+            self.catboost_params = {'iterations': 2500,
+                                    'learning_rate': 0.061824808908317305,
+                                    'depth': 10,
+                                    'l2_leaf_reg': 6.111025595991382,
+                                    'random_strength': 7.1230012835574765,
+                                    'loss_function': 'MAPE',
+                                    'eval_metric': 'MAPE',
+                                    'early_stopping_rounds': 100}
+        else:
+            self.catboost_params = catboost_params
+        
+        if cat_cols is None:
+            self.cat_cols = ['transport_type', 'origin_kma', 'destination_kma']
+        else:
+            self.cat_cols = cat_cols
 
-    def predict(self, x):
-        return [self.mean_rate] * len(x)
+        self.catboost_regressor = CatBoostRegressor(**self.catboost_params)
+
+
+    def fit_predict(self, X_train, y_train, X_valid):
+
+        #check if the X_train and y_train are the same length
+        assert len(X_train) == len(y_train), "X_train and y_train are not the same length"
+
+        #check if X_train and X_valid are pandas dataframes and y_train is a pandas series
+        assert isinstance(X_train, pd.DataFrame), "X_train is not a pandas dataframe"
+        assert isinstance(X_valid, pd.DataFrame), "X_valid is not a pandas dataframe"
+        assert isinstance(y_train, pd.Series), "y_train is not a pandas series"
+
+        #check that index of X_train and y_train are the same
+        assert X_train.index.equals(y_train.index), "Index of X_train and y_train are not the same"
+
+        #check if the columns in X_train and X_valid are the same and self.cat_cols are in X_train
+        assert X_train.columns.equals(X_valid.columns), "X_train and X_valid columns are not the same"
+        assert all(col in X_train.columns for col in self.cat_cols), "Not all categorical columns are in X_train"
+
+        #check that index of X_train and y_train are the same
+        assert X_train.index.equals(y_train.index), "Index of X_train and y_train are not the same"
+
+        #Fit CatBoost and get predictions
+        print("Fitting CatBoost...")
+        train_pool = Pool(X_train, y_train, cat_features=self.cat_cols)
+        self.catboost_regressor.fit(train_pool, early_stopping_rounds=100, verbose=200)
+        catboost_pred = self.catboost_regressor.predict(X_valid)
+
+        print("Getting TabPFN predictions...")
+        #Get TabPFN predictions. Don't need to train TabPFN as it is "in-context" learning
+        grouped_valid = X_valid.groupby(["origin_kma", "destination_kma"])
+        tabpfn_pred = pd.Series(index=X_valid.index, dtype=float)
+
+        # Loop over each group => single (origin, destination) pair
+        for (origin, destination), group_df in tqdm(grouped_valid):
+            #Grab the suitable training subset
+            train_subset = self.__get_suitable_train_subset__(X_train, origin, destination, max_rows=5000)
+            y_subset = y_train.loc[train_subset.index]
+
+            #Get predictions for this group
+            reg = TabPFNRegressor()
+            reg.fit(train_subset, y_subset)
+            group_pred = reg.predict(group_df)
+
+            #Store predictions in 'predictions' for these specific indices
+            tabpfn_pred.loc[group_df.index] = group_pred
+
+        blended_pred = 0.25 * catboost_pred + 0.75 * tabpfn_pred
+
+        return blended_pred
+
+
+    def __get_suitable_train_subset__(self, train_df, origin, destination, max_rows=5000):
+        """
+        Returns a subset of train_df up to 'max_rows':
+        1) All rows with the same (origin, destination).
+        2) If < max_rows, fill with the most recent rows from the same origin
+        but different destinations, until you reach max_rows.
+        """
+        # A) All with same (origin, destination)
+        same_ori_dst = train_df[
+            (train_df['origin_kma'] == origin) &
+            (train_df['destination_kma'] == destination)
+        ]
+        
+        if len(same_ori_dst) >= max_rows:
+            # If enough rows, just pick the most recent 'max_rows'
+            subset = same_ori_dst.tail(max_rows)
+        else:
+            # Use all same (origin, destination)
+            subset = same_ori_dst
+            
+            # B) Fill up with same origin, other destinations
+            remainder = max_rows - len(subset)
+            same_origin_other_dst = train_df[
+                (train_df['origin_kma'] == origin) &
+                (train_df['destination_kma'] != destination)
+            ].sort_values(['year', 'month', 'day', 'hour'])  # ensure chronological
+            
+            additional = same_origin_other_dst.tail(remainder)
+            subset = pd.concat([subset, additional])
+        
+        return subset
--- train.py
+++ train.py
@@ -4,34 +4,60 @@ import numpy as np
 from model import Model
 
 
+def add_time_features(df, date_col='pickup_date'):
+    '''
+    Add simple time features to the dataframe
+    '''
+    df[date_col] = pd.to_datetime(df[date_col])
+    df['year'] = df[date_col].dt.year
+    df['month'] = df[date_col].dt.month
+    df['day'] = df[date_col].dt.day
+    df['hour'] = df[date_col].dt.hour
+    max_date = pd.to_datetime('2022-10-01')
+    df['months_to_max'] = ((max_date.year - df['year']) * 12 + (max_date.month - df['month'])) + 2
+    df.drop(date_col, axis=1, inplace=True)  
+    return df
+
+
 def loss(real_rates, predicted_rates):
     return np.average(abs(predicted_rates / real_rates - 1.0)) * 100.0
 
 
 def train_and_validate():
-    df = pd.read_csv('dataset/train.csv')
-    model = Model()
-    model.fit(df, df.rate)
+    train_df = pd.read_csv('dataset/train.csv')
+    valid_df = pd.read_csv('dataset/validation.csv')
+    train_df = add_time_features(train_df)
+    train_df = train_df[train_df['year'] >= 2021] # use only 2021 and 2022 data
+    valid_df = add_time_features(valid_df)
+    X_train = train_df.drop('rate', axis=1)
+    y_train = train_df['rate']
+    X_valid = valid_df.drop('rate', axis=1)
+    y_valid = valid_df['rate']
 
-    df = pd.read_csv('dataset/validation.csv')
-    predicted_rates = model.predict(df)
-    mape = loss(df.rate, predicted_rates)
+    model = Model()
+    predicted_rates = model.fit_predict(X_train, y_train, X_valid)
+
+    mape = loss(y_valid, predicted_rates)
     mape = np.round(mape, 2)
     return mape
 
 
 def generate_final_solution():
     # combine train and validation to improve final predictions
-    df = pd.read_csv('dataset/train.csv')
-    df_val = pd.read_csv('dataset/validation.csv')
-    df = df.append(df_val).reset_index(drop=True)
+    train_df = pd.read_csv('dataset/train.csv')
+    test_df = pd.read_csv('dataset/test.csv')
+    train_df = add_time_features(train_df)
+    train_df = train_df[train_df['year'] >= 2021] # use only 2021 and 2022 data
+    test_df = add_time_features(test_df)
+    X_train = train_df.drop('rate', axis=1)
+    y_train = train_df['rate']
 
     model = Model()
-    model.fit(df, df.rate)
+    predicted_rates = model.fit_predict(X_train, y_train, test_df)
 
     # generate and save test predictions
     df_test = pd.read_csv('dataset/test.csv')
-    df_test['predicted_rate'] = model.predict(df_test)
+    df_test['predicted_rate'] = predicted_rates
     df_test.to_csv('dataset/predicted.csv', index=False)
 
 
